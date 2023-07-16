74d825cd9ece50b7dd9307bc015bf59e4c38bac5
mickoissicko <mickeysidd@proton.me>
2023-07-16 10:14:23 +0200

    Major bug fix for blink

---
+++ src/blink.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <Winsock2.h>
+#include <conio.h> // Include conio.h for _kbhit() and getch()
+#include "WinDivert.h"
+
+#define MAXBUF 0xFFFF
+
+// Function to add a delay to the packet (simulate latency)
+void add_delay(uint8_t *packet, uint32_t delay_ms) {
+    Sleep(delay_ms); // Sleep for the specified delay time (in milliseconds)
+}
+
+int main() {
+    HANDLE handle;
+    char filter[] = "outbound";
+    char packet[MAXBUF];
+    UINT packet_len;
+    WINDIVERT_ADDRESS addr;
+    int running = 1;
+
+    // Initialize WinDivert
+    handle = WinDivertOpen(filter, WINDIVERT_LAYER_NETWORK, 0, 0);
+    if (handle == INVALID_HANDLE_VALUE) {
+        fprintf(stderr, "Error opening WinDivert: %d\n", GetLastError());
+        return 1;
+    }
+
+    // Enable non-blocking console input
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD prevMode;
+    GetConsoleMode(hInput, &prevMode);
+    SetConsoleMode(hInput, prevMode & ~ENABLE_PROCESSED_INPUT);
+
+    printf("Clumsy is running. Press Y to stop, T to start.\n");
+
+    while (running) {
+        // Check if a key is pressed
+        if (_kbhit()) {
+            char ch = _getch();
+            if (ch == 'Y' || ch == 'y') {
+                running = 0;
+                printf("Clumsy stopped.\n");
+            } else if (ch == 'T' || ch == 't') {
+                running = 1;
+                printf("Clumsy resumed.\n");
+            }
+        }
+
+        if (!running) {
+            Sleep(100); // Small delay when not running to avoid high CPU usage
+            continue;
+        }
+
+        if (!WinDivertRecv(handle, packet, sizeof(packet), &addr, &packet_len)) {
+            fprintf(stderr, "Error while receiving packet: %d\n", GetLastError());
+            continue;
+        }
+
+        // Simulate latency by adding a 2-second delay to the packet
+        add_delay((uint8_t*)packet, 2000);
+
+        if (!WinDivertSend(handle, packet, packet_len, &addr, NULL)) {
+            fprintf(stderr, "Error while sending packet: %d\n", GetLastError());
+            continue;
+        }
+    }
+
+    // Restore the original console input mode
+    SetConsoleMode(hInput, prevMode);
+
+    WinDivertClose(handle);
+    return 0;
+}
